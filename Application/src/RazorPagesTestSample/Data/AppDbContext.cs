25b71c15f49314b9300cc512e6ea58560f57ea8f
sharlenecera <89477399+sharlenecera@users.noreply.github.com>
2024-06-06 15:18:13 +0000

    changed app name

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -15,6 +15,7 @@ namespace RazorPagesTestSample.Data
 
         public virtual DbSet<Message> Messages { get; set; }
 
+
         #region snippet1
         public async virtual Task<List<Message>> GetMessagesAsync()
         {
