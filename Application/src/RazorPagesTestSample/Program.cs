3752cbe7794c304b9be350a1cdccbbfac1499a5f
David <30800534+david-p-mitchell@users.noreply.github.com>
2024-06-06 14:08:21 +0000

    trying again

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -12,7 +12,6 @@ namespace RazorPagesTestSample
     {
         public static void Main(string[] args)
         {
-            
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
